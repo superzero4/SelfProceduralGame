afadc4559db962fec3bf9d41ea3791f07169aadd
superzero4 <78915984+superzero4@users.noreply.github.com>
2024-05-02 19:36:48 +0200

    Create simple entries

---
+++ Assets/Scripts/SubmitToChat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using GoogleApis.Example;
+using GoogleApis.GenerativeLanguage;
+using Unity.Barracuda;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(TMPro.TMP_InputField))]
+public class SubmitToChat : MonoBehaviour
+{
+    [SerializeField] private string _prePrompt;
+    private TMPro.TMP_InputField _submit;
+    private static BasicChatExample _chat;
+
+    private void Awake()
+    {
+        _submit = GetComponent<TMPro.TMP_InputField>();
+        _submit.onSubmit.AddListener(ToChat);
+        if (_chat == null)
+        {
+            _chat = gameObject.AddComponent<BasicChatExample>();
+        }
+    }
+
+    private async void ToChat(string s)
+    {
+        await _chat.SendRequest(s,_prePrompt);
+    }
+}
