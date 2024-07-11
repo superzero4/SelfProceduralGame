83de4b5c35cbafc88f82fcffde07171d619327f7
Eliot <fromenteliot@gmail.com>
2024-07-11 12:27:37 +0200

    Passage data scène en scène

--- Assets/Scripts/Game/InitFromHistory.cs
+++ Assets/Scripts/Game/InitFromHistory.cs
@@ -1,6 +1,7 @@
 using Game;
 using TMPro;
 using UnityEngine;
+using FinalData = Game.FinalData;
 
 public class InitFromHistory : MonoBehaviour
 {
@@ -9,18 +10,31 @@ public class InitFromHistory : MonoBehaviour
     
     [SerializeField] private GameObject buttonParent;
     [SerializeField] private GameObject buttonPrefab;
+
+    private FinalData data;
     
     void Start()
     {
-        Building[] buidlings = history.Get(0).data.buildings;
+        data = FindObjectOfType<CrossSceneInfo>().data;
+
+        Building[] buidlings;
+        if (data.Equals(default(FinalData)))
+        {
+            Debug.LogWarning("Using history");
+            buidlings = history.Get(0).data.buildings;
+        }
+        else
+        {
+            buidlings = data.buildings;
+        }
         
         for (var i = 0; i < buidlings.Length; i++)
         {
+            // Todo: assigner unités à ce building
+            
             Building b = buidlings[i];
             GameObject newItem = Instantiate(buttonPrefab, buttonParent.transform);
             newItem.GetComponentInChildren<TMP_Text>().text = b.name;
-            
-            
         }
     }
 
